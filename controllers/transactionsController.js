56d5d296f6615e4c5d65668810f98b2a058f267f
Carlitos <carlitosdutan@pursuit.org>
2024-02-29 13:14:17 -0500

    show, delete fully functional

--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -2,9 +2,25 @@ const express = require("express");
 
 const transactions = express.Router();
 
-const transactionsArray = require("../models/transactions.models");
+let transactionsArray = require("../models/transactions.models");
 
 transactions.get("/", (req, res) => {
+  res.json({ transactions: transactionsArray });
+});
+
+transactions.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const index = transactionsArray.findIndex(
+    (transaction) => transaction.id === +id
+  );
+  res.json({ transaction: transactionsArray[index] });
+});
+
+transactions.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  transactionsArray = transactionsArray.filter(
+    (transaction) => transaction.id !== +id
+  );
   res.json({ transactions: transactionsArray });
 });
 
