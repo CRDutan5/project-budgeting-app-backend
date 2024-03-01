df128ef0b0509c4248be70ce1819e3968eeb45c3
Carlitos <carlitosdutan@pursuit.org>
2024-02-29 19:34:01 -0500

    POST route fixed

--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -8,6 +8,13 @@ transactions.get("/", (req, res) => {
   res.json({ transactions: transactionsArray });
 });
 
+transactions.post("/", (req, res) => {
+  const newId = transactionsArray[transactionsArray.length - 1].id + 1;
+  req.body.id = newId;
+  transactionsArray.push(req.body);
+  res.json({ transactions: transactionsArray });
+});
+
 transactions.get("/:id", (req, res) => {
   const { id } = req.params;
   const index = transactionsArray.findIndex(
